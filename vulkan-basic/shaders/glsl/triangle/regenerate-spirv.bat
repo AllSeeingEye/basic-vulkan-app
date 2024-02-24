1a0a85ce54775b1d046dfc5144d34e5ef8e12d45
Dmitry Khudorozhkov <khrr@email.cz>
2024-02-24 09:40:58 +0100

    Even better SPIR-V recompilation script

--- vulkan-basic/shaders/glsl/triangle/regenerate-spirv.bat
+++ vulkan-basic/shaders/glsl/triangle/regenerate-spirv.bat
@@ -1,16 +1,5 @@
 @echo off
 
-for %%f in (*.frag) do (
-    echo %%~nf
-    glslangvalidator -V %%~nf.frag  -o %%~nf.frag.spv
-)
-
-for %%f in (*.vert) do (
-    echo %%~nf
-    glslangvalidator -V %%~nf.vert  -o %%~nf.vert.spv
-)
-
-for %%f in (*.comp) do (
-    echo %%~nf
-    glslangvalidator -V %%~nf.comp  -o %%~nf.comp.spv
+for %%f in (*.frag, *.vert, *.comp) do (
+    glslangvalidator -V %%~f  -o %%~f.spv
 )
