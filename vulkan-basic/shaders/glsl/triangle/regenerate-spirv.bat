ea56b428a67e143803660663b0f46d23e6ced5cc
Dmitry Khudorozhkov <khrr@email.cz>
2024-02-27 18:38:49 +0100

    Ideal SPIR-V recompilation script

--- vulkan-basic/shaders/glsl/triangle/regenerate-spirv.bat
+++ vulkan-basic/shaders/glsl/triangle/regenerate-spirv.bat
@@ -1,5 +1,12 @@
 @echo off
 
-for %%f in (*.frag, *.vert, *.comp) do (
+REM .vert Vertex shader
+REM .frag Fragment shader
+REM .comp Compute shader
+REM .geom Geometry shader
+REM .tccs Tessellation Control Shader
+REM .tecs Tessellation Evaluation Shader
+
+for %%f in (*.frag, *.vert, *.comp, *.geom, *.tccs, *.tecs) do (
     glslangvalidator -V %%~f  -o %%~f.spv
 )
